54fd4ada945c53f5a854c262d0ec6e1a9c0f4e77
tyodev <deve@tyo.com.au>
2017-05-03 13:18:56 +1000

    the tyoki app can start properly now

--- src/main/java/au/com/tyo/wiki/wiki/ArticleParsingInterface.java
+++ src/main/java/au/com/tyo/wiki/wiki/ArticleParsingInterface.java
@@ -1,7 +1,9 @@
 package au.com.tyo.wiki.wiki;
 
-public abstract interface ArticleParsingInterface {
-	public void onFinishParsing(WikiPage page);
+public interface ArticleParsingInterface {
 
-	public void onParsingError(String string);
+	void onFinishParsing(WikiPage page);
+
+	void onParsingError(String string);
+
 }
